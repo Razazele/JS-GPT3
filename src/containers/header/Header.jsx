33a592307adf7b9f643938d07204758c837d1097
Razazele <pablo_verdugoj@hotmail.com>
2021-12-25 16:36:27 -0300

    Agregado Componente Brand,Feature y containers header,whatgpt3

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,17 +7,17 @@ const Header = () => {
   return (
     <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+      <h1 className="gradient__text">Construyamos algo asombroso con GPT-3 OpenAI</h1>
       <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
       <div className="gpt3__header-content__input">
-        <input type="email" placeholder="Your Email Address" />
-        <button type="button">Get Started</button>
+        <input type="email" placeholder="Tu correo electronico" />
+        <button type="button">Empieza Aqui</button>
       </div>
 
       <div className="gpt3__header-content__people">
         <img src={people} alt="people" />
-        <p>1,600 people requested access a visit in last 24 hours</p>
+        <p>1,600 personas han visitado este sitio en las ultimas 24 horas</p>
       </div>
     </div>
 
